dfae5992e73ff30849e1a1b3a3888ae7854e43d3
Moazam05 <salmanmoazam08@gmail.com>
2022-03-05 17:11:21 +0500

    Folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+      <button className='bg-orange-500 font-bold text-white rounded p-2'>
+        Click Me
+      </button>
     </div>
   );
 }
