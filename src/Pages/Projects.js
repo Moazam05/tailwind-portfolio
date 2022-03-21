766fe709f2286b04bd9357d9bc20b38c81154b1a
Moazam05 <salmanmoazam08@gmail.com>
2022-03-21 23:08:05 +0500

    project section

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
+import { projectsData } from '../resources/Projects';
 
 const Projects = () => {
   return (
@@ -26,6 +27,31 @@ const Projects = () => {
           <h1 className='text-8xl uppercase md:text-3xl'>The GAME Is...</h1>
           <h1 className='text-8xl uppercase md:text-3xl'>Consistency</h1>
         </div>
+      </div>
+
+      <div className='mt-20 grid md:grid-cols-1 grid-cols-3 items-center justify-center gap-10 mx-20 md:mx-5'>
+        {projectsData.map((project, index) => {
+          const { image, title } = project;
+          return (
+            <div key={index}>
+              <div className='relative p-10 border-2 border-gray-400 text-center rounded-tr-3xl rounded-bl-3xl'>
+                <img
+                  src={image}
+                  alt={title}
+                  className='w-full h-72 text-center'
+                />
+                <div className='absolute inset-0 flex items-center justify-center rounded-tr-3xl rounded-bl-3xl flex-col opacity-0 bg-black hover:opacity-80'>
+                  <div className='text-3xl font-semibold text-white'>
+                    {title}
+                  </div>
+                  <button className='border-2 border-white rounded py-2 px-10 text-white font-medium hover:bg-green-500 mt-5'>
+                    Demo
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </Layout>
   );
---
+++ src/resources/Projects.jsx
@@ -0,0 +1,50 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: 'Burger Shop',
+    description:
+      'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+    link: '/',
+    image: '/burger.jpg',
+  },
+  {
+    id: 2,
+    title: 'Hotel App',
+    description:
+      'est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla',
+    link: '/',
+    image: '/hotel.jpg',
+  },
+  {
+    id: 3,
+    title: 'Car Rental',
+    description:
+      'et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut',
+    link: '/',
+    image: '/car.jpg',
+  },
+  {
+    id: 4,
+    title: 'Ecommerce Ease',
+    description:
+      'ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit',
+    link: '/',
+    image: '/ecommerce.jpg',
+  },
+  {
+    id: 5,
+    title: 'Gym Fitness',
+    description:
+      'repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor neque',
+    link: '/',
+    image: '/hotel.jpg',
+  },
+  {
+    id: 6,
+    title: 'Newspaper',
+    description:
+      'ut aspernatur corporis harum nihil quis provident sequimollitia nobis aliquid molestiaeperspiciatis et ea nemo ab reprehenderit accusantium quasvoluptate dolores velit et doloremque molestiae',
+    link: '/',
+    image: '/ecommerce.jpg',
+  },
+];
